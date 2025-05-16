b475ce14daf5250a742fafbacf792f352b9eff58
RodrigoFilipeFaria <rodrigofilipefaria@gmail.com>
2025-05-16 16:15:33 +0100

    cliente pu_send_message feito

--- cliente.c
+++ cliente.c
@@ -2,31 +2,34 @@
  * O cliente comunica via TCP para pedir uma nova configuração ao Servidor.
  * */
 
+#include <arpa/inet.h>
+#include <assert.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h>
 #include <strings.h>
 #include <unistd.h>
-#include <assert.h>
 
 #include "powerudp.h"
 
+#include <arpa/inet.h>
+#include <endian.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
-#include <stdbool.h>
+#include <sys/time.h>
 
-#define SV_IP   "193.137.101.1"
+#define SV_IP "193.137.101.1"
 #define SV_PORT 443
 
-#define PSK     "337b8d2c1e132acd75171f1acf0e73b20bc9541720d5003813f59ef0ad51f86f"
+#define PSK "337b8d2c1e132acd75171f1acf0e73b20bc9541720d5003813f59ef0ad51f86f"
 
-PU_ConfigMessage config = {0}; 
+PU_ConfigMessage config = {0};
 static uint64_t current_seq = 0;
 
 typedef struct {
@@ -37,24 +40,25 @@ typedef struct {
 global_t global;
 
 /* PowerUDP */
-int  pu_init_protocol(const char *server_ip, int server_port, const char *psk);
+int pu_init_protocol(const char *server_ip, int server_port, const char *psk);
 void pu_close_protocol();
 
-int  pu_request_protocol_config(PU_ConfigMessage new_config);
-int  pu_send_message(const char *destination, const char *message, int len);
-int  pu_receive_message(char *buffer, int bufsize);
-int  pu_get_last_message_stats(int *retransmissions, int *delivery_time);
+int pu_request_protocol_config(PU_ConfigMessage new_config);
+int pu_send_message(const char *destination, const char *message, int len);
+int pu_receive_message(char *buffer, int bufsize);
+int pu_get_last_message_stats(int *retransmissions, int *delivery_time);
 void pu_inject_packet_loss(int probability);
 
-static bool config_is_set() {
-
-  PU_ConfigMessage empty_config = {0};
-  if (memcmp((void*) &empty_config, (void*) &config, sizeof(PU_ConfigMessage))) {
-    return true;
-  }
-  
-  return false;
-}
+// static bool config_is_set() {
+//
+//   PU_ConfigMessage empty_config = {0};
+//   if (memcmp((void *)&empty_config, (void *)&config,
+//              sizeof(PU_ConfigMessage))) {
+//     return true;
+//   }
+//
+//   return false;
+// }
 
 /* Init protocol */
 int pu_init_protocol(const char *server_ip, int server_port, const char *psk) {
@@ -76,7 +80,8 @@ int pu_init_protocol(const char *server_ip, int server_port, const char *psk) {
   }
 
   /* ligar ao servidor */
-  if (connect(global.sv_sock, (struct sockaddr*) &sv_addr, sizeof(sv_addr)) < 0) {
+  if (connect(global.sv_sock, (struct sockaddr *)&global.sv_addr,
+              sizeof(global.sv_addr)) < 0) {
     fputs("TCP connection failed", stderr);
     close(global.sv_sock);
     return -1;
@@ -92,7 +97,8 @@ int pu_init_protocol(const char *server_ip, int server_port, const char *psk) {
   /* obter resposta do servidor */
   char resposta_serializada[sizeof(PU_ConfigMessage)];
   memset(&resposta_serializada, 0, sizeof(PU_ConfigMessage));
-  int recvlen = read(global.sv_sock, &resposta_serializada, sizeof(PU_ConfigMessage));
+  int recvlen =
+      read(global.sv_sock, &resposta_serializada, sizeof(PU_ConfigMessage));
   if (recvlen < 0) {
     fputs("Failed to recieve config message from server.", stderr);
     close(global.sv_sock);
@@ -104,35 +110,48 @@ int pu_init_protocol(const char *server_ip, int server_port, const char *psk) {
     fputs("PSK was incorrect!", stderr);
     close(global.sv_sock);
     return -1;
-  }
-  else if (recvlen < (int) sizeof(PU_ConfigMessage)) {
+  } else if (recvlen < (int)sizeof(PU_ConfigMessage)) {
     fputs("Recieved malformed config.", stderr);
     close(global.sv_sock);
     return -1;
   }
 
-  memcpy(&config, resposta_serializada, sizeof(PU_ConfigMessage) );
+  memcpy(&config, resposta_serializada, sizeof(PU_ConfigMessage));
   puts("Successfuly retrieved config!");
   return 0;
 }
 
 int pu_send_message(const char *destination, const char *message, int len) {
 
-  const char* host = destination;
-  char* seperator = strchr(host, ':');
+  const char *host = destination;
+  char *seperator = strchr(host, ':');
   if (seperator == NULL) {
     fputs("[ERROR] pu_send_message: does not contain ':' seperator.", stderr);
     return -1;
   }
 
-  int port = atoi(seperator+1);
+  int port = atoi(seperator + 1);
   if (port <= 0) {
     fputs("[ERROR] pu_send_message: port must be greater than 0", stderr);
     return -1;
   }
 
+  char ip[INET_ADDRSTRLEN];
+  size_t ip_len = seperator - destination;
+  strncpy(ip, destination, ip_len);
+  ip[ip_len] = '\0';
+
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  struct sockaddr_in dest_addr;
+  dest_addr.sin_family = AF_INET;
+  dest_addr.sin_port = htons(port);
+  inet_pton(AF_INET, ip, &dest_addr.sin_addr);
   int retries = 0;
 
+  if (inet_pton(AF_INET, ip, &dest_addr.sin_addr) != 1) {
+    fputs("Invalid IP address format.\n", stderr);
+    return -1;
+  }
   assert(config.base_timeout > 0);
 
   struct timeval tv;
@@ -148,7 +167,7 @@ int pu_send_message(const char *destination, const char *message, int len) {
   while (retries < config.max_retries) {
     // Preencher header
     struct timeval now;
-    // gettimeofday(&now, NULL);
+    gettimeofday(&now, NULL);
 
     header.timestamp = now.tv_sec * 1000000 + now.tv_usec;
     header.sequence = current_seq;
@@ -157,13 +176,13 @@ int pu_send_message(const char *destination, const char *message, int len) {
     /* Calcular checksum */
     memcpy(packet, &header, sizeof(header));
     memcpy(packet + sizeof(header), message, len);
-    header.checksum = htobe16(pu_checksum_helper(packet, sizeof(header) + len));
+    header.checksum = htons(pu_checksum_helper(packet, sizeof(header) + len));
     memcpy(packet, &header, sizeof(header));
 
     // Enviar
     ssize_t sent = sendto(sockfd, packet, sizeof(packet), 0,
-                          (struct sockaddr*)dest_addr, sizeof(*dest_addr));
-    if (sent != sizeof(packet)) {
+                          (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+    if ((size_t)sent != sizeof(packet)) {
       perror("sendto() failed");
       return -1;
     }
@@ -180,17 +199,19 @@ int pu_send_message(const char *destination, const char *message, int len) {
       struct sockaddr_in src_addr;
       socklen_t addr_len = sizeof(src_addr);
 
-      ssize_t recvd = recvfrom(sockfd, &ack_header, sizeof(ack_header), 0, (struct sockaddr*)&src_addr, &addr_len);
+      ssize_t recvd = recvfrom(sockfd, &ack_header, sizeof(ack_header), 0,
+                               (struct sockaddr *)&src_addr, &addr_len);
 
-      if (recvd == sizeof(ack_header) && 
-        (PU_IS_ACK(ack_header.flag)) && 
-        be32toh(ack_header.sequence) == current_seq) {
+      if (recvd == sizeof(ack_header) && (PU_IS_ACK(ack_header.flag)) &&
+          (uint64_t)ntohl(ack_header.sequence) == current_seq) {
         current_seq++;
+        close(sockfd);
         return 0; // Sucesso
       }
     } else if (ready == 0) {
       retries++;
-      fprintf(stderr, "Timeout, retrying (%d/%d)\n", retries, config.max_retries);
+      fprintf(stderr, "Timeout, retrying (%d/%d)\n", retries,
+              config.max_retries);
     } else {
       perror("select() failed");
       return -1;
@@ -201,28 +222,31 @@ int pu_send_message(const char *destination, const char *message, int len) {
   return -1;
 }
 
-int  pu_receive_message(char *buffer, int bufsize);
+int pu_receive_message(char *buffer, int bufsize);
 
-int main(int argc, char *argv[] ) {
+int main(int argc, char *argv[]) {
 
   /* Verificar argumentos */
   char *addr = argv[1];
   int port = atoi(argv[2]);
 
-  // char *msg = argv[2];
+  char *msg = argv[3];
 
   if (argc != 4) {
     perror("cliente [hostname] [port] [mensagem]");
     exit(EXIT_FAILURE);
   }
 
-  /* Registar no servidor */ 
+  /* Registar no servidor */
   if (pu_init_protocol(addr, port, PSK) < 0) {
     fputs("[ERROR] Failed to initialize PowerUDP\n", stderr);
     exit(EXIT_FAILURE);
   }
+  char destination[64];
 
-  // pu_send_message(addr, , int len)
+  snprintf(destination, sizeof(destination), "%s:%d", addr, port);
+
+  pu_send_message(destination, msg, strlen(msg));
 
   /* Fechar registo no servidor */
   // pu_close_protocol();
